ce41264a90af8214ed81a01fb60894fbb117d78c
ArloTheGreat <159732916+ArloTheGreat@users.noreply.github.com>
2024-05-02 10:18:02 +1200

    Created first quiz

---
+++ main.py
@@ -0,0 +1,11 @@
+#Ask the user their name and store it
+name = input("What's your name?")
+#Greet the user and introduce the quiz
+print("welcome to the quiz,", name)
+print("This quiz is about crocodiles.")
+#Ask the user a question
+input("Do you like crocodiles?")
+#Tell them the correct answer
+print("The only correct answer is yes.")
+#End the quiz
+print("Thanks for playing, goodbye")
