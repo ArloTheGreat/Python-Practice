a4c352e082ac8eb264773bad2768910a57b1ddbb
ArloTheGreat <159732916+ArloTheGreat@users.noreply.github.com>
2024-05-06 11:57:18 +1200

    Update main.py

--- main.py
+++ main.py
@@ -1,11 +1,98 @@
 #Ask the user their name and store it
 name = input("What's your name?")
 #Greet the user and introduce the quiz
-print("welcome to the quiz,", name)
-print("This quiz is about crocodiles.")
-#Ask the user a question
-input("Do you like crocodiles?")
-#Tell them the correct answer
-print("The only correct answer is yes.")
+print("welcome to this quiz,", name)
+print("This quiz is about animals.")
+print("Please answer the questions using a simple 'Yes' or 'No'")
+#Ask the user question 1
+answer = input("Do you like Crocodiles?")
+#Tell them if the correct answer
+if answer == "Yes":
+    print("That's cool")
+if answer == "No":
+    print("...ok")
+if answer != "Yes" or "No":
+    print("Thats cool")
+#Ask the user question 2
+answer2 = input("Do you like Sharks?")
+#Tell them if the correct answer
+if answer2 == "Yes":
+    print("Nice answer")
+if answer2 == "No":
+    print("...ok")
+if answer2 != "Yes" "no":
+    print("Thats cool")
+#Ask the user question 3
+answer3 = input("Do you like Rats?")
+#Tell them if the correct answer
+if answer3 == "No":
+    print("Rats are bad")
+if answer3 == "Yes":
+    print("...why")
+if answer3 != "Yes" "no":
+    print("Thats cool")
+#Ask the user question 4
+answer4 = input("Do you like Dogs?")
+#Tell them if the correct answer
+if answer4 == "Yes":
+    print("You are correct")
+if answer4 == "No":
+    print("You are wrong")
+if answer4 != "Yes" "no":
+    print("Thats cool")
+#Ask the user question 5
+answer5 = input("Do you like Cats?")
+#Tell them if the correct answer
+if answer5 == "Yes":
+    print("You are correct")
+if answer5 == "No":
+    print("You are wrong")
+if answer5 != "Yes" "no":
+    print("Thats cool")
+#Ask the user question 6
+answer6 = input("Do you like Snakes?")
+#Tell them if the correct answer
+if answer6 == "Yes":
+    print("Snakes are very noodle like")
+if answer6 == "No":
+    print("Snakes are very noodle like")
+if answer6 != "Yes" "no":
+    print("Thats cool")
+#Ask the user question 7
+answer7 = input("Do you like Pigs?")
+#Tell them if the correct answer
+if answer7 == "Yes":
+    print("Bacon, bacon, bacon")
+if answer7 == "No":
+    print("Bacon, bacon, bacon")
+if answer7 != "Yes" "no":
+    print("Thats cool")
+#Ask the user question 8
+answer8 = input("Do you like Lions?")
+#Tell them if the correct answer
+if answer8 == "Yes":
+    print("Lions are very dangerous")
+if answer8 == "No":
+    print("That's cool")
+if answer8 != "Yes" "no":
+    print("Thats cool")
+#Ask the user question 9
+answer9 = input("Do you like Birds?")
+#Tell them if the correct answer
+if answer9 == "Yes":
+    print("ok.")
+if answer9 == "No":
+    print("ok.")
+if answer9 != "Yes" "no":
+    print("Thats cool")
+#Ask the user question 10
+answer10 = input("Do you like Frogs?")
+#Tell them if the correct answer
+if answer10 == "Yes":
+    print("Very bouncy boys")
+if answer10 == "No":
+    print("Very bouncy boys")
+if answer10 != "Yes" "no":
+    print("Thats cool")
 #End the quiz
 print("Thanks for playing, goodbye")
