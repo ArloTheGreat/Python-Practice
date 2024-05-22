ece975fdbc87f0945f3defffd38224c10b591237
ArloTheGreat <159732916+ArloTheGreat@users.noreply.github.com>
2024-05-23 10:21:15 +1200

    Update main.py

--- main.py
+++ main.py
@@ -1,3 +1,5 @@
+#random
+import random
 #The users score
 score = 1
 #Ask the user their name and store it
@@ -7,124 +9,127 @@ print("welcome to this quiz,", name)
 print("My name is Bob")
 print("This quiz is about animals.")
 print("Please answer the questions using a simple 'Yes' or 'No'")
+#Good and Bad commejts
+GOOD_COMMENT = ["Good job", "Keep it up", "Fantastic"]
+BAD_COMMENT = ["At least try to answer correctly next time", "Try using more then 0.0001% of your brain", "Dont give up"] 
 #Ask the user question 1
 answer = input("Do you like Crocodiles?").lower()
 #Tell them if the correct answer
 if answer == "Yes".lower():
-    print("That's cool")
+    print(random.choice(GOOD_COMMENT))
     score *= 1
 elif answer == "":
     print("TRY ANSWERING THE NEXT QUESTION")
 else:
-    print("...ok")
+    print(random.choice(BAD_COMMENT))
     score += 1
 print("I like crocodiles.")
 #Ask the user question 2
 answer = input("Do you like Sharks?").lower()
 #Tell them if the correct answer
 if answer == "Yes".lower():
-    print("Nice answer" )
+    print(random.choice(GOOD_COMMENT))
     score *= 2
 elif answer == "":
     print("TRY ANSWERING THE NEXT QUESTION")
 else:
-    print("...ok")
+    print(random.choice(BAD_COMMENT))
     score += 2
 print("I like Sharks.")
 #Ask the user question 3
-answer = input("Do you like Rats?").lower()
+answer = input("Do you like Molerats?").lower()
 #Tell them if the correct answer
 if answer == "Yes".lower():
-    print("...why do you like Rats")
-    score += 3
+    print(random.choice(GOOD_COMMENT))
+    score *= 3
 elif answer == "":
     print("TRY ANSWERING THE NEXT QUESTION")
 else:
-    print("good answer")
-    score *= 3
-print("I don't like Rats.")
+    print(random.choice(BAD_COMMENT))
+    score += 3
+print("I like Molerats.")
 #Ask the user question 4
 answer = input("Do you like Dogs?").lower()
 #Tell them if the correct answer
 if answer == "Yes".lower():
-    print("You are correct")
+    print(random.choice(GOOD_COMMENT))
     score *= 4
 elif answer == "":
     print("TRY ANSWERING THE NEXT QUESTION")
 else:
-    print("You are wrong")
+    print(random.choice(BAD_COMMENT))
     score += 4
 print("I like Dogs.")
 #Ask the user question 5
 answer = input("Do you like Cats?").lower()
 #Tell them if the correct answer
 if answer == "Yes".lower():
-    print("You are correct")
+    print(random.choice(GOOD_COMMENT))
     score *= 5
 elif answer == "":
     print("TRY ANSWERING THE NEXT QUESTION")
 else:
-    print("You are wrong")
+    print(random.choice(BAD_COMMENT))
     score += 5
 print("I like Cats.")
 #Ask the user question 6
 answer = input("Do you like Snakes?").lower()
 #Tell them if the correct answer
 if answer == "Yes".lower():
-    print("Snakes are very noodle like")
+    print(random.choice(GOOD_COMMENT))
     score *= 6
 elif answer == "":
     print("TRY ANSWERING THE NEXT QUESTION")
 else:
-    print("Snakes are very noodle like")
+    print(random.choice(BAD_COMMENT))
     score += 6
-print("I don't like Snakesb though.")
+print("I like snakes")
 #Ask the user question 7
 answer = input("Do you like Pigs?").lower()
 #Tell them if the correct answer
 if answer == "Yes".lower():
-    print("Bacon, bacon, bacon")
+    print(random.choice(GOOD_COMMENT))
     score *= 7
 elif answer == "":
     print("TRY ANSWERING THE NEXT QUESTION")
 else:
-    print("Bacon, bacon, bacon")
+    print(random.choice(BAD_COMMENT))
     score += 7
 print("I like Pigs.")
 #Ask the user question 8
 answer = input("Do you like Lions?").lower()
 #Tell them if the correct answer
 if answer == "Yes".lower():
-    print("Lions are very dangerous")
+    print(random.choice(GOOD_COMMENT))
     score *= 8
 elif answer == "":
     print("TRY ANSWERING THE NEXT QUESTION")
 else:
-    print("That's cool")
+    print(random.choice(BAD_COMMENT))
     score += 8
 print("I like Lions.")
 #Ask the user question 9
-answer = input("Do you like Birds?").lower()
+answer = input("Do you like Bevers?").lower()
 #Tell them if the correct answer
 if answer == "Yes".lower():
-    print("ok.")
-    score += 9
+    print(random.choice(GOOD_COMMENT))
+    score *= 9
 elif answer == "":
     print("TRY ANSWERING THE NEXT QUESTION")
 else:
-    print("ok.")
-    score *= 9
-print("I don't like Birds.")
+    print(random.choice(BAD_COMMENT))
+    score += 9
+print("I like Bevers")
 #Ask the user question 10
 answer = input("Do you like Frogs?").lower()
 #Tell them if the correct answer
 if answer == "Yes".lower():
-    print("Very bouncy boys")
+    print(random.choice(GOOD_COMMENT))
     score *= 10
 elif answer == "":
     print("Dumb")
 else:
-    print("Very bouncy boys")
+    print(random.choice(BAD_COMMENT))
     score += 10
 print("Frogs are my favorite.")
 #End the quiz
